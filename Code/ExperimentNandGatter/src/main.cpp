6609f984ec15fd02c08ccb668a13f4b2b22acf59
UnHold (Alex) <alex@hold.re>
2023-03-07 17:55:11 +0100

    added setup code for spi transfer

--- Code/ExperimentNandGatter/src/main.cpp
+++ Code/ExperimentNandGatter/src/main.cpp
@@ -1,9 +1,36 @@
 #include <Arduino.h>
+#include <SPI.h>
 
-void setup() {
-  // put your setup code here, to run once:
+SPIClass *vspi = new SPIClass(VSPI);
+
+#define MASK 0b0011111111110000
+#define GAIN_1 0x2000
+#define ACTIVE 0x1000
+
+#define INPUT_DAC_CS 17
+
+void setVoltage(uint8_t cs, uint8_t value)
+{
+    vspi->beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0));
+    digitalWrite(cs, LOW);
+    uint16_t data = value << 4;
+    data |= ACTIVE;
+    data &= MASK;
+    vspi->transfer16(data);
+    digitalWrite(cs, HIGH);
+    vspi->endTransaction();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+    pinMode(INPUT_DAC_CS, OUTPUT);
+    digitalWrite(INPUT_DAC_CS, HIGH);
+    vspi->begin();
+
+    setVoltage(INPUT_DAC_CS, 128);
+}
+
+void loop()
+{
+    // put your main code here, to run repeatedly:
 }
